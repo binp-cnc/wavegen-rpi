6ce27c25bded18b5628ac6a22fac500f76bf3b01
Alexey Gerasev <alexey.gerasev@gmail.com>
2017-11-21 16:04:15 +0700

    spi batch response test

--- main.c
+++ main.c
@@ -2,14 +2,15 @@
 #include <stdint.h>
 #include <wiringPiSPI.h>
 
+#define BATCH_SIZE 16
+
 #define NCHANS  2
 
 #define CMD_NONE        0x00
 #define CMD_MOVE        0x10 // [nsteps:32, bdelay:32, edelay:32]*NCHANS
 
 #define CMD_NONE_SIZE   0
-#define CMD_MOVE_SIZE   4
-
+#define CMD_MOVE_SIZE   ((4+4+4)*NCHANS)
 
 int main(int argc, char *argv[]) {
 	int st = 0;
@@ -17,22 +18,40 @@ int main(int argc, char *argv[]) {
 	if (fd < 0) {
 		perror("cannot init spi");
 	}
-	uint8_t data[1+CMD_MOVE_SIZE] = {CMD_MOVE,0x55,0x50,0x05,0x55};
-	printf("spi tx start\n");
+	uint8_t data[BATCH_SIZE];
 	
-	st = wiringPiSPIDataRW(0, data, 1);
-	if (st < 0) {
-		perror("cannot tx header");
-	} else {
-		printf("header tx done\n");
+	printf("spi txrx start\n");
+	int i;
+	int k = 0;
+	for (;;) {
+		int j;
+		data[0] = CMD_MOVE;
+		for (j = 0; j < BATCH_SIZE - 1; ++j) {
+			data[1 + j] = 0x00;
+		}
+
+		st = wiringPiSPIDataRW(0, data, BATCH_SIZE);
+		if (st < 0) {
+			perror("txrx error");
+			break;
+		}
+
+		/*
+		if (data[1] == 0xFF) {
+			k += 1;
+		} else {
+			printf("error %02X\n", data[0]);
+		}
+		printf("sent: %d,\n received: %d\n", k, *((uint32_t*)(data + 4)));
+		*/
+
+		for (j = 0; j < 8; ++j) {
+			printf("%02X", data[j]);
+		}
+		printf("\n");
+
+		delayMicroseconds(250000);
 	}
-	delayMicroseconds(1000);
-	
-	st = wiringPiSPIDataRW(0, data + 1, CMD_MOVE_SIZE);
-	if (st < 0) {
-		perror("cannot tx data");
-	} else {
-		printf("data tx done\n");
-	}
+
 	return 0;
 }
